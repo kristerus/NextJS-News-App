97116b806f60f1c6b2d5c1d6262fa55d7d1a82d9
Kristian Dhimitri <kristerus@protonmail.com>
2022-06-05 21:13:33 +0200

    Delete Downloads/Face-Detection-JavaScript-master(1) directory

--- Downloads/Face-Detection-JavaScript-master(1)/modules.js
+++
@@ -1,6 +0,0 @@
-const names = require("./secondary");
-const sayHi = require("./tertiary");
-const data = require("./4-alternativemodule");
-require("./5-mind-grenade");
-sayHi(names.second);
-sayHi(names.third);
