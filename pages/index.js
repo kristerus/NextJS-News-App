9f0f1e8d58a2612dc4c9343cce68b19b8d4afab3
Kristian Dhimitri <kristerus@protonmail.com>
2022-06-05 21:22:35 +0200

    added alles

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { Toolbar } from "../components/Toolbar";
+
+export default function Home() {
+  return (
+    <div className="page-container">
+      <Toolbar />
+      <div className={styles.main}>
+        <h1>Next.Js News App</h1>
+        <h3>Your one stop shop for the newest News</h3>
+      </div>
+    </div>
+  );
+}
